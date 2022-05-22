68fbe12b0981f0d32a8c91d3898fc9ac3a7ccfca
Labib2003 <labib.salimi30@gmail.com>
2022-05-22 13:16:18 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.436gp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db('tools-manufacturer').collection('tools');
+        
+        // all tools api
+        app.get('/tools', async(req, res) => {
+            const result = await toolsCollection.find({}).toArray();
+            res.send(result);
+        })
+    }
+    finally { }
+};
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 });
+
+// toolAdmin
+// QEDBtsyMTxUXhcJ8
