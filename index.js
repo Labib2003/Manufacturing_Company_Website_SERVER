aa75322944c383cc7e3c03558144ea70e211bb21
Labib2003 <labib.salimi30@gmail.com>
2022-05-23 15:44:44 +0600

    get product by id api added

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.436gp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,10 +16,17 @@ async function run() {
         await client.connect();
         const toolsCollection = client.db('tools-manufacturer').collection('tools');
         const reviewsCollection = client.db('tools-manufacturer').collection('reviews');
-        
+
         // get all tools
-        app.get('/tools', async(req, res) => {
+        app.get('/tools', async (req, res) => {
             const result = await toolsCollection.find({}).toArray();
+            res.send(result);
+        });
+
+        // get a specific tool by id
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await toolsCollection.findOne({ _id: ObjectId(id) });
             res.send(result);
         });
 
@@ -43,3 +49,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+// https://tools-manufacturer.herokuapp.com
